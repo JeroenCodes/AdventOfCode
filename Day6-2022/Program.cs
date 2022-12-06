a5c4cff62a24d3b6a835ac44bb40fe91ee3fe6e8
jbijl005 <jeroen.maurits.bijl@pwc.com>
2022-12-06 07:58:48 +0100

    Slight cleanup

--- Day6-2022/Program.cs
+++ Day6-2022/Program.cs
@@ -6,7 +6,8 @@ char[] characters = input.ToCharArray();
 //part one
 for (int v = 4; v < characters.Length; v++)
 {
-    var previousFour = new List<char>() { characters[v - 1], characters[v - 2], characters[v - 3], characters[v - 4] };
+    var start = v; var end = v - 4;
+    var previousFour = characters[end..start];
 
     if (previousFour.Distinct().Count() == 4)
     {
@@ -20,7 +21,8 @@ for (int v = 4; v < characters.Length; v++)
 
 for (int v = 14; v < characters.Length; v++)
 {
-    var previousFourteen = new List<char>() { characters[v - 1], characters[v - 2], characters[v - 3], characters[v - 4], characters[v - 5], characters[v - 6], characters[v - 7], characters[v - 8], characters[v - 9], characters[v - 10], characters[v - 11], characters[v - 12], characters[v - 13], characters[v - 14] };
+    var start = v; var end = v - 14;
+    var previousFourteen = characters[end..start];
 
     if (previousFourteen.Distinct().Count() == 14) Console.WriteLine(v);
 }
